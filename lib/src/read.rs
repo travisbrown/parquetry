af84b1371779c6cf3e46a391c8dcb1dd2bb11d51
Travis Brown <travisrobertbrown@gmail.com>
2024-09-16 11:41:24 +0200

    Move writer code

---
+++ lib/src/read.rs
@@ -0,0 +1,24 @@
+use crate::error::Error;
+use parquet::record::{reader::RowIter, Row};
+use std::marker::PhantomData;
+
+pub enum SchemaIter<T> {
+    Failed(Option<Error>),
+    Streaming {
+        rows: RowIter<'static>,
+        _item: PhantomData<T>,
+    },
+}
+
+impl<T: TryFrom<Row, Error = Error>> Iterator for SchemaIter<T> {
+    type Item = Result<T, Error>;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        match self {
+            Self::Failed(error) => error.take().map(|error| Err(error)),
+            Self::Streaming { rows, .. } => rows
+                .next()
+                .map(|row| row.map_err(Error::from).and_then(|row| row.try_into())),
+        }
+    }
+}
